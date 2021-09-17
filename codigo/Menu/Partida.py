ee2fb65e82a303f025ddffd4982aac8bad386c08
jmanuares <jmanuares@gmail.com>
2021-09-16 23:18:18 -0300

    Parte de islas completa

--- codigo/Menu/Partida.py
+++ codigo/Menu/Partida.py
@@ -5,9 +5,17 @@ class Partida(Boton):
     def __init__(self, pos, font, text, screen, color, colorRec, colorfondo,partida):
         Boton.__init__(self, pos, font, text, screen, color, colorRec, colorfondo)
         self.partida = partida
+        self.borrar = None
 
     def getPartida(self):
         return self.partida
 
     def setPartida(self,partida):
         self.partida = partida
+
+    def setBorrar(self,borrar):
+        self.borrar = borrar
+
+    def getBorrar(self):
+        return self.borrar
+        
--- codigo/pruebita.py
+++ codigo/pruebita.py
@@ -191,59 +191,52 @@ def Islasini():
     alto = 480
     screen = pygame.display.set_mode((ancho, alto))
     clock = pygame.time.Clock()
-    color = (112,140,104)
     confirmarBor = False
-    borrar = False
-
     islas = True
+
+    #Tiempo para fondo
+    timerFondo = 4
+    pygame.time.set_timer(timerFondo,1000)
+
+
+    partida = None
+    start= ""
+    
+    def degradado():
+        # Pantalla para el FADE de cambio de pantalla
+        pantallaNegra = pygame.Surface((ancho, alto))
+        pantallaNegra.fill((0, 0, 0))
+        #Cambia rapidamente entre la pantalla y la pantalla negra cada vez mas lento
+        #hasta quedar todo negro
+        for alpha in range(0, 300):
+            pantallaNegra.set_alpha(alpha)
+            dibujarMenu()
+            screen.blit(pantallaNegra, (0, 0))
+            pygame.display.update()
+            pygame.time.delay(5)
+
 
     partidas = []
     slotsPartidas = 3
+    #Meter los archivos de "Info" en "archivos"
     archivos = os.listdir("Info")
+    #Buscar las partidas que hay
     for partida in archivos:
         partes = partida.split(".")
         partidas.append(partes[0])
 
-    cantPartidas = len(partidas) 
-
+    #Definir los slots vacios de partidas
     nuevasPartidas = slotsPartidas
     for partida in partidas:
         nuevasPartidas -= 1
 
 
-    
-    #Tiempo para fondo
-    timerFondo = 4
-    pygame.time.set_timer(timerFondo,1000)
-    #Dibujar fondo
-        
-    cuadro = Helper.CUADRO
-    cuadro = pygame.transform.scale(cuadro, (ancho,150))
-
-    
+    #Crear los recuadros para las islas
     recuadrosIslas = []
-    
     for i in range(0,3):
         recuadrosIslas.append(pygame.Rect((10, 10 + (155*i)), (ancho- 20, 150)))
 
-    partida = None
-    start= ""
-    
-    def degradado(color):
-        # Pantalla para el degradado
-        pantallaNegra = pygame.Surface((ancho, alto))
-        pantallaNegra.fill((0, 0, 0))
-        for alpha in range(0, 300):
-            pantallaNegra.set_alpha(alpha)
-            dibujarMenu(color)
-            screen.blit(pantallaNegra, (0, 0))
-            pygame.display.update()
-            pygame.time.delay(5)
-          
-    def start(partida):
-        degradado(color)
-        cargarJuego(partida)
-        
+
     botonesIniciar = []
     botonesNew = []
     botonesBorrar = []
@@ -251,38 +244,49 @@ def Islasini():
     i = 0
     for partida in partidas:
         i += 1
-        botonesIniciar.append(Boton((ancho-120,(i*155)),20,"  Elegir Isla  ",screen,"white","black","brown"))
+        #Por cada partida que haya crear sus respectivos botones iniciar y borrar
+        botonesIniciar.append(Partida((ancho-150,(i*155)),20,"  Elegir Isla  ",screen,"white","black","brown",partida))
         botonesBorrar.append(Partida((ancho-10,(i*155)),20,"  Borrar Isla  ",screen,"white","black","brown",partida))
     
-    
+    #Por cada slot de partida vacio crear un boton "Nueva Partida"
     for i in range(3-nuevasPartidas+1, 3+1):
         botonesNew.append(Boton((ancho-10,(i*155)),20,"Nueva Partida",screen,"white","black","brown"))
 
+    #Ordenar los tipos de botones en una lista para que sean mas faciles de dibujar
     botones = [botonesIniciar , botonesBorrar, botonesNew]
+
+    #Crear el cuadro de la advertencia de Borrar
     advertencia = PopUp(ancho, alto)
 
+    #Crear los botones para la advertencia de Borrar
     botonAceptar = Boton((ancho / 2 , alto / 2 + 45), 24, "   Si   ", screen, "black", "black", "grey")
     botonRechazar = Boton((ancho / 2 + 85 , alto / 2 + 45), 24, "   No   ", screen, "black", "black", "grey")
     
 
-    def dibujarMenu(color):
+    def dibujarMenu():
+        orange = (210,137,8)
         for recuadro in recuadrosIslas:
-            pygame.draw.rect(screen, color, recuadro)
+            pygame.draw.rect(screen, orange, recuadro)
 
         nombrePartida = []
         for partida in partidas:
             nombrePartida.append(Helper.FUENTE(30).render(partida, True, "black"))
+
         for i in range(nuevasPartidas):
             nombrePartida.append(Helper.FUENTE(30).render("No hay partida creada", True, "black"))
+
         i = 0
+        #Dibujar los nombres de las partidas
         for nombre in nombrePartida:
             screen.blit(nombre,(20, 50 + (i*155) + 10)) 
             i += 1
 
+        #Dibujar los botones de cada tipo
         for listas in botones:
             for boton in listas:  
                 boton.dibujarBoton(1)
 
+        #Dibujar la advertencia si se clickeo borrar
         if confirmarBor == True:
             popArriba = "Seguro que desea eliminar"
             popAbajo = "Esta isla"
@@ -300,45 +304,59 @@ def Islasini():
                 islas = False
 
 
-        i = 0
-        for boton in botonesIniciar:      
-            if boton.click(event):
-                partida = partidas[i]
-                start(partida)
-                islas = False
-            i += 1
+        #Por cada boton "Iniciar" checkear si clickean en el
+        if confirmarBor == False:
+            for boton in botonesIniciar:      
+                if boton.click(event):
+                    #Iniciar la partida asociada al boton
+                    degradado()
+                    cargarJuego(boton.getPartida())
+                    islas = False
 
 
-        i = 0
+        #Por cada boton "Borrar" checkear si clickean en el
         for boton in botonesBorrar:
             if boton.click(event):
+                #se debera llamar a la advertencia
                 confirmarBor = True
-                partida = boton.getPartida()
-                
-            i += 1
+                #Crear un holder para la info de ese boton
+                botonHolder = boton
+
         
         if botonAceptar.click(event):
-            remove(f'info/{partida}.json')
-            nombrePartida[i] = "Partida no creada" 
+            confirmarBor = False
+            botonHolder.setBorrar(True)
 
 
         if botonRechazar.click(event):
             confirmarBor = False
 
-        for boton in botonesNew:
-            if boton.click(event):
-                degradado(color)
+        #Por cada boton "Nueva partida" checkear si clickean en el
+        if confirmarBor == False:
+            for boton in botonesNew:
+                if boton.click(event):
+                    #salir de esta pantalla e ir al menu de creacion
+                    degradado()
+                    islas = False
+                    menu()
+
+
+        try:
+            #Si se confirma que se quiere borrar borrara
+            if botonHolder.getBorrar() == True:
+                remove(f'info/{partida}.json')
                 islas = False
-                menu()
-                    
-            
+                Islasini()
+        except:
+            pass
+
+
         # Dibujar todo
         clock.tick(60)     
         screen.fill("black")
-        dibujarMenu(color)   
+        dibujarMenu()   
         pygame.display.update()
 
-        
 
-            
-cargarJuego("prueba")
+
+Islasini()
